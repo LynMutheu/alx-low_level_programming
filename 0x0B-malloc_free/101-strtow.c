f2cec5bf65876835565a78f92dc0898b12b98a30
MutheuLinet <muneenilinet@gmail.com>
2022-04-05 17:58:13 +0300

    Change file

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -75,23 +75,3 @@ char **strtow(char *str)
 
 	return (matrix);
 }
-	str = malloc(sizeof(char) * (len + 1));
-
-	if (str == NULL)
-		return (NULL);
-
-	k = 0;
-
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j] != '\0'; j++)
-		{
-			str[k] = av[i][j];
-			k++;
-		}
-		str[k] = '\n';
-		k++;
-	}
-
-	return (str);
-}
